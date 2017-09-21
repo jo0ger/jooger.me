5dadf36eb33d08964848c02d7b0e17bb8a9e5e81
Jooger <zzy1198258955@163.com>
2017-09-21 19:42:31 +0800

    [update] update logo favicon

--- build/main.js
+++ build/main.js
@@ -311,7 +311,7 @@ module.exports = {
     title: 'Jooger.me - 凡心所向，一往无前',
     titleTemplate: '%s - Jooger.me',
     meta: [{ charset: 'utf-8' }, { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' }, { 'http-equiv': 'Cache-Control', content: 'no-transform' }, { 'http-equiv': 'Cache-Control', content: 'no-siteapp' }, { 'http-equiv': 'cleartype', content: 'on' }, { name: 'viewport', content: 'width=device-width, initial-scale=1.0, user-scalable=no' }, { name: 'MobileOptimized', content: '320' }, { name: 'HandheldFriendly', content: 'True' }, { name: 'apple-mobile-web-app-capable', content: 'yes' }, { name: 'author', content: 'zzy1198258955@163.com' }, { hid: 'keywords', name: 'keywords', content: 'Jooger,Jooger.me,Blog,FE,前端' }, { hid: 'description', name: 'description', content: '凡心所向，一往无前' }],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }, { rel: 'stylesheet', href: '//at.alicdn.com/t/font_268993_tu4k21igcspgmn29.css' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   css: ['normalize.css', 'swiper/dist/css/swiper.css', 'gitalk/dist/gitalk.css', { src: '~assets/stylus/index.styl', lang: 'stylus' }],
   loading: '~/components/Common/PageLoading',
