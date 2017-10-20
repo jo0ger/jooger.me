be3d3831f7093e079755c878f41b1c76eb7d853c
Jooger <zzy1198258955@163.com>
2017-10-20 12:17:35 +0800

    [update] update footer

--- client/components/Layout/Footer/index.vue
+++ client/components/Layout/Footer/index.vue
@@ -1,11 +1,18 @@
 <template>
   <footer class="footer" :style="footerStyle">
     <div class="wrapper">
-      Powered by 
-      <a class="link" target="_blank" href="https://github.com/nuxt/nuxt.js">nuxt.js</a>、
-      <br v-if="mobileLayout">
-      Developed by 
-      <a class="link" target="_blank" :href="'https://github.com/jo0ger'">{{ me.name }}</a>
+      <p>版权所有 © 2017 {{ me.name }}. 保留所有权利
+        <a href="http://www.miitbeian.gov.cn" target="_blank" class="record">
+          豫ICP备17040457号
+        </a>
+      </p>
+      <p>
+        Powered by 
+        <a class="link" target="_blank" href="https://github.com/nuxt/nuxt.js">nuxt.js</a>、
+        <br v-if="mobileLayout">
+        Developed by 
+        <a class="link" target="_blank" :href="'https://github.com/jo0ger'">{{ me.name }}</a>
+      </p>
     </div>
   </footer>
 </template>
@@ -38,7 +45,8 @@
 
   .footer {
     width 100%
-    padding 40px 0
+    padding 40px 0 120px
+    color $text-color-secondary
 
     .wrapper {
       position relative
@@ -46,6 +54,7 @@
       max-width $content-max-width
       margin 0 auto
       text-align center
+      font-size 12px
 
       @media (max-width: 1640px) {
         padding 0 100px
@@ -63,6 +72,29 @@
         padding 0 30px
       }
 
+      p {
+        margin-bottom 10px
+      }
+
+      .record {
+        margin-left 10px
+      }
+
+    }
+  }
+</style>
+
+<style lang="stylus">
+  @import '~assets/stylus/_var'
+  
+  .dark-page {
+    .footer {
+      color $text-color-secondary-dark
+
+      .link {
+        color $text-color-secondary-dark
+        border-color $text-color-secondary-dark
+      }
     }
   }
 </style>
