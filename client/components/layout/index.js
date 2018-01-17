4caa5f66b17812858a9b2aedf0b72f5dea6064cd
Jooger <zzy1198258955@163.com>
2018-01-18 01:28:37 +0800

    [update] update git config ignorecase

---
+++ client/components/layout/index.js
@@ -0,0 +1,15 @@
+/**
+ * @desc Layout components entrance
+ * @author Jooger <iamjooger@gmail.com>
+ * @date 20 Dec 2017
+ */
+
+'use strict'
+
+export { default as AppHeader } from './Header'
+export { default as AppAside } from './Aside'
+export { default as AppFooter } from './Footer'
+export { default as AppPageLoader } from './PageLoader'
+export { default as AppHero } from './Hero'
+export { default as AppBackground } from './Background'
+export { default as AppTool } from './Tool'
