8592bc96fe4d66212298ecb356b09528e9d8d42c
jo0ger <iamjooger@gmail.com>
2018-01-18 01:28:37 +0800

    [update] update git config ignorecase

---
+++ client/components/layout/Footer/index.vue
@@ -0,0 +1,67 @@
+<style lang="stylus">
+  @import 'index'
+</style>
+
+<template>
+  <footer class="app-footer">
+    <div class="wrapper">
+      <div class="menu-list">
+        <nuxt-link v-for="menu in menus"
+          ref="menu"
+          class="menu-item"
+          :key="menu.key"
+          :to="{ name: menu.key }"
+          exact>
+          {{ menu.title }}
+        </nuxt-link>
+      </div>
+      <div class="social-list">
+        <a :href="social.url" class="social-item" target="_blank" rel="noopener"
+          :class="[`social-${social.icon}`]"
+          v-for="social in socials"
+          :key="social.icon"
+          :title="social.title">
+          <i class="icon" :class="[`icon-${social.icon}`]"></i>
+        </a>
+      </div>
+      <div class="copyright">
+        <span>版权所有 © 2017 <nuxt-link to="/about">{{ blogger.name }}</nuxt-link>. 保留所有权利</span>
+        <a v-if="!mobileLayout" href="http://www.miitbeian.gov.cn" target="_blank" rel="noopener" class="record">
+          豫ICP备17040457号
+        </a>
+      </div>
+      <div class="power-by">
+        Powered by 
+        <a class="link" target="_blank" rel="noopener" href="https://github.com/nuxt/nuxt.js">nuxt.js</a>、
+        Design by 
+        <a class="link" target="_blank" rel="noopener" :href="'https://github.com/jo0ger'">{{ blogger.name }}</a>
+      </div>
+      <div class="logo">
+        <nuxt-link to="/">
+          <i class="icon icon-logo"></i>
+        </nuxt-link>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  import config from '@@/app.config'
+
+  export default {
+    name: 'AppFooter',
+    data () {
+      return {
+        menus: config.constant.menus,
+        socials: config.constant.socials
+      }
+    },
+    computed: {
+      ...mapGetters({
+        blogger: 'user/blogger',
+        mobileLayout: 'app/mobileLayout'
+      })
+    }
+  }
+</script>
