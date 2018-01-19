4f0cecb4b4e218a6b3908911d995988ce2767479
jo0ger <iamjooger@gmail.com>
2018-01-20 00:04:17 +0800

    [update] update the fucking filename case

---
+++ client/components/common/index.js
@@ -0,0 +1,18 @@
+/**
+ * @desc Common components entrance
+ * @author Jooger <iamjooger@gmail.com>
+ * @date 20 Dec 2017
+ */
+
+'use strict'
+
+export { default as Message } from './Message'
+export { default as Moment } from './Moment'
+export { default as Card } from './Card'
+export { default as Tab } from './Tab'
+export { default as Loading } from './Loading'
+export { default as Affix } from './Affix'
+export { default as Tag } from './Tag'
+export { default as Comment } from './Comment'
+export { default as Editor } from './Editor'
+export * from './Article'
