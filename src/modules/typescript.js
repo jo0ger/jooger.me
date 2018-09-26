6e6e54da88edb2b3a590c3f7a12e85fc2d325ce5
Jooger <iamjooger@gmail.com>
2018-09-27 02:17:36 +0800

    upgrade: upgrade nuxt to 2.0

--- src/modules/typescript.js
+++ src/modules/typescript.js
@@ -1,3 +1,5 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
 module.exports = function () {
   // Add .ts extension for store, middleware and more
   this.nuxt.options.extensions.push("ts")
@@ -15,11 +17,12 @@ module.exports = function () {
     const tsLoader = {
       loader: 'ts-loader',
       options: {
+        transpileOnly: true,
         appendTsSuffixTo: [/\.vue$/]
       }
     }
     // Add TypeScript loader
-    config.module.rules.push(tslintLoader)
+    // config.module.rules.push(tslintLoader)
     config.module.rules.push(
       Object.assign({
           test: /((client|server)\.js)|(\.tsx?)$/
@@ -30,15 +33,22 @@ module.exports = function () {
     // Add TypeScript loader for vue files
     for (let rule of config.module.rules) {
       if (rule.loader === "vue-loader") {
-        rule.options.loaders.ts = tsLoader
+        rule.options.loaders = {
+          ts: 'ts-loader!tslint-loader'
+        }
       }
     }
     // Add .ts extension in webpack resolve
-    if (
-      config.resolve.extensions.indexOf(".ts") ===
-      -1
-    ) {
-      config.resolve.extensions.push(".ts")
+    if (config.resolve.extensions.indexOf(".ts") === -1) {
+      config.resolve.extensions.push(".ts");
     }
+
+    config.plugins.push(new ForkTsCheckerWebpackPlugin({
+      vue: true,
+      tslint: true,
+      formatter: 'codeframe',
+      // https://github.com/TypeStrong/ts-loader#happypackmode-boolean-defaultfalse
+      checkSyntacticErrors: process.env.NODE_ENV === 'production'
+    }))
   })
 }
