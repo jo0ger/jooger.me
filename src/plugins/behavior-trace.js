a513cc42ea4bb062e18e8f11c7213de211dd6946
Jooger <zzy1198258955@163.com>
2019-06-10 21:59:23 +0800

    fix: lint error

--- src/plugins/behavior-trace.js
+++ src/plugins/behavior-trace.js
@@ -52,11 +52,16 @@ function setupLogRocket () {
 }
 
 function setupGrowingIO () {
-  !function (e, t, n, g, i) {
+  (function (e, t, n, g, i) {
     e[i] = e[i] || function () {
       (e[i].q = e[i].q || []).push(arguments)
-    }, n = t.createElement("script"), tag = t.getElementsByTagName("script")[0], n.async = 1, n.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + g, tag.parentNode.insertBefore(n, tag)
-  }(window, document, "script", "assets.growingio.com/2.1/gio.js", "gio");
-  gio('init', '9c24b896d23d1d67', {});​
-  gio('send');
+    }
+    n = t.createElement("script")
+    tag = t.getElementsByTagName("script")[0]
+    n.async = 1
+    n.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + g
+    tag.parentNode.insertBefore(n, tag)
+  })(window, document, "script", "assets.growingio.com/2.1/gio.js", "gio")
+  window.gio('init', '9c24b896d23d1d67', {})
+  window.gio('send')
 }
