6c2c5afb834f9e7606c2791ae3e8d005c1005b48
jo0ger <iamjooger@gmail.com>
2019-06-20 17:36:58 +0800

    fix: script error

--- src/plugins/behavior-trace.js
+++ src/plugins/behavior-trace.js
@@ -80,9 +80,9 @@ function setupMouseFlow () {
 
 function setupSmartLook () {
   window.smartlook||(function(d) {
-  var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+  var o=window.smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
   var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
   c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
   })(document);
-  smartlook('init', '8059a279f9daeae36e35dcceff5a23c469ec5d5e');
+  window.smartlook('init', '8059a279f9daeae36e35dcceff5a23c469ec5d5e');
 }
